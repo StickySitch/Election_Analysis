258ab20b072b140f5469a86a5bacc3656c07f912
Jack Jones <fuzzydunlop19@gmail.com>
2021-11-06 02:57:19 -0700

    Added candidate vote count & percentage

--- PyPoll.py
+++ PyPoll.py
@@ -1,25 +1,52 @@
 import csv
 import os
 
-
+#Instantiate variable to hold 'election_results.csv' data
 csvFile = os.path.join('Resources','election_results.csv')
+
+#Instantiate variable to hold output file
 saveFile = os.path.join('Analysis', 'election_analysis.txt')
+
+#Instantiating varable to hold total vote count
+totalVotes = 0
+#Instantiating list to hold candidate optoins
+candidateOptions = []
+#Instantiating dict to hold the votes for each candidate
+candidateVotes = {}
+
 
 #Open the election results and read the file.
 with open(csvFile) as election_data:
-    # To Do: Read and analyze data
+
+    #Reading 'election_results.csv' data, skipping the header row
     fileReader = csv.reader(election_data)
     headers = next(fileReader)
-    print(headers)
 
+    #looping through rows
+    for row in fileReader:
+        totalVotes += 1
+
+        #Assigning candidate name to variable
+        candidateName = row[2]
+        #Adds candidate name if not already in the 'candidateOptions' list
+        if candidateName not in candidateOptions:
+            candidateOptions.append(candidateName)
+
+            #Instantiating 'candidateOptions' list variables to 0
+            candidateVotes[candidateName] = 0
+        candidateVotes[candidateName] +=1
+
+
+#Loop to calculate percentages
+for candidateName in candidateVotes:
     
-    #for row in fileReader:
-       
+    votes = candidateVotes[candidateName]
+    #Calculates the percentage of votes for each candidate
+    votePercentage = float(votes) / float(totalVotes) * 100
 
+    print(f'{candidateName}: recieved {votePercentage:.1f}% of the vote.')
 
-
-
-
+#Opening and writing to output file
 with open(saveFile, 'w') as txt_file:
     txt_file.write('Counties in the Election\n---------------------------\nArapahoe\nDenver\nJefferson')
     
